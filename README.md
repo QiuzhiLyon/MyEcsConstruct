38c7702d98b3342f6c3d99af6c52013060f00715
QiuzhiLyon <zhiyueonline2023@gmail.com>
2025-05-19 12:14:03 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Welcome to your CDK TypeScript project
+
+This is a blank project for CDK development with TypeScript.
+
+The `cdk.json` file tells the CDK Toolkit how to execute your app.
+
+## Useful commands
+
+* `npm run build`   compile typescript to js
+* `npm run watch`   watch for changes and compile
+* `npm run test`    perform the jest unit tests
+* `npx cdk deploy`  deploy this stack to your default AWS account/region
+* `npx cdk diff`    compare deployed stack with current state
+* `npx cdk synth`   emits the synthesized CloudFormation template
+
+## Bootstrap
+cdk bootstrap aws://095254076971/us-east-1
+
+cdk destroy
+
+aws elbv2 describe-load-balancers
+
+http://myecsc-myfar-kooyzjewb1im-741086476.us-east-1.elb.amazonaws.com/
+
+
+npm install @aws-cdk/aws-ecs @aws-cdk/aws-ec2 @aws-cdk/aws-rds @aws-cdk/aws-secretsmanager @aws-cdk/aws-lambda @aws-cdk/aws-iam @aws-cdk/aws-logs
---
+++ lib/my_ecs_construct-stack.ts
@@ -0,0 +1,128 @@
+import * as cdk from 'aws-cdk-lib';
+import {Construct} from 'constructs';
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as rds from "aws-cdk-lib/aws-rds";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
+import * as tasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
+import * as stepfunctions from 'aws-cdk-lib/aws-stepfunctions';
+import * as custom from 'aws-cdk-lib/custom-resources';
+
+import * as ecr from "aws-cdk-lib/aws-ecr-assets";
+import * as path from 'path';
+
+export class MyEcsConstructStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, "MyVpc", {
+      maxAzs: 3 // Default is all AZs in region
+    });
+
+    const cluster = new ecs.Cluster(this, "MyCluster", {
+      vpc: vpc
+    });
+
+    const db = new rds.DatabaseInstance(this, 'OnlineShoppingDB', {
+      engine: rds.DatabaseInstanceEngine.mysql({ version: rds.MysqlEngineVersion.VER_8_0_32 }),
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
+      vpc,
+      credentials: rds.Credentials.fromPassword('rootroot', cdk.SecretValue.unsafePlainText('rootroot')),
+      allocatedStorage: 20,
+      databaseName: 'online_shopping',
+      publiclyAccessible: true,
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+
+    });
+
+    const initTaskDef = new ecs.FargateTaskDefinition(this, 'InitTaskDef', {
+      memoryLimitMiB: 512,
+      cpu: 256,
+    });
+
+    initTaskDef.addContainer('InitContainer', {
+      image: ecs.ContainerImage.fromAsset(path.join(__dirname, '../init_task'),
+          {
+            platform: ecr.Platform.LINUX_AMD64,
+          }
+      ),
+      logging: new ecs.AwsLogDriver({ streamPrefix: 'InitTask' }),
+      environment: {
+        DB_HOST: db.dbInstanceEndpointAddress,
+        DB_USER: 'rootroot',
+        DB_PASSWORD: 'rootroot',
+      }
+    });
+
+    const runTask = new tasks.EcsRunTask(this, 'Run Init SQL Task', {
+      integrationPattern: stepfunctions.IntegrationPattern.RUN_JOB,
+      cluster,
+      taskDefinition: initTaskDef,
+      launchTarget: new tasks.EcsFargateLaunchTarget(),
+      assignPublicIp: true, // or false depending on your networking setup
+      securityGroups: [ec2.SecurityGroup.fromSecurityGroupId(this, 'SG', vpc.vpcDefaultSecurityGroup)],
+      subnets: { subnetType: ec2.SubnetType.PUBLIC },
+    });
+
+    const stateMachine = new stepfunctions.StateMachine(this, 'InitDbStateMachine', {
+      definitionBody: stepfunctions.DefinitionBody.fromChainable(runTask),
+      timeout: cdk.Duration.minutes(5),
+    });
+
+    // Custom resource to start the State Machine execution
+    const startExecution = new custom.AwsCustomResource(this, 'StartInitTaskExecution', {
+      onCreate: {
+        service: 'StepFunctions',
+        action: 'startExecution',
+        parameters: {
+          stateMachineArn: stateMachine.stateMachineArn,
+        },
+        physicalResourceId: custom.PhysicalResourceId.of('InitTaskExecution'),
+      },
+      policy: custom.AwsCustomResourcePolicy.fromSdkCalls({
+        resources: [stateMachine.stateMachineArn],
+      }),
+    });
+
+    // Create a load-balanced Fargate service and make it public
+    const fargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
+      cluster: cluster, // Required
+      cpu: 256, // Default is 256
+      desiredCount: 3, // Default is 1
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromAsset(
+            '/Users/Peng/Workspace/OnlineShopping_06',
+            {
+              platform: ecr.Platform.LINUX_AMD64
+            }
+        ),
+        containerPort: 8070,
+        environment: {
+          DB_HOST: db.dbInstanceEndpointAddress
+        },
+      },
+      memoryLimitMiB: 512, // Default is 512
+      publicLoadBalancer: true // Default is true
+    });
+
+    // Step 4: Ensure app runs after init
+    fargateService.node.addDependency(startExecution);
+
+    db.connections.allowFromAnyIpv4(ec2.Port.tcp(3306), 'Allow public MySQL access');
+
+    // 5. Allow ECS to connect to RDS
+    db.connections.allowFrom(
+        fargateService.service.connections,
+        ec2.Port.tcp(3306)
+    );
+
+    // 6. Output the RDS endpoint and LB URL
+    new cdk.CfnOutput(this, 'RDSEndpoint', {
+      value: db.dbInstanceEndpointAddress,
+    });
+
+    new cdk.CfnOutput(this, 'LoadBalancerURL', {
+      value: fargateService.loadBalancer.loadBalancerDnsName,
+    });
+  }
+}
